c8b5296e144ecbc08bc80a8a1706760ebe7a750f
Omar Cossio <omar119mce@gmail.com>
2020-12-18 12:02:14 -0500

    edited appointment.js

--- models/appointment.js
+++ models/appointment.js
@@ -22,7 +22,7 @@ module.exports = function(sequelize, DataTypes) {
     },
 
     time: {
-      type: Sequelize.TIME,
+      type: DataTypes.TIME,
       allowNull: false,
       unique: true
     }
